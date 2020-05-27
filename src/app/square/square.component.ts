fc2fa3e99995392b51a0af169b8c62a78e6daa5d
Zeeshan Syed <zsyed89@gmail.com>
2020-05-26 23:21:15 -0400

    Init

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <button>{{ value }}</button>
+  `,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+})
+export class SquareComponent {
+
+  @Input() value: 'X' | 'O';
+
+}
